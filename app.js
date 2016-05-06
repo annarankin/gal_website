3ebd64686d992c37f04ba08101dbca013a001792
malinatran <tran.malina@gmail.com>
2016-05-06 17:48:30 -0400

    Changed nav, font, section titles

---
+++ app.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {
+  showNavLogo();
+};
+
+function showNavLogo() {
+  var scrollHeight = $(this).scrollTop();
+  if (scrollHeight > 400) {
+    $('nav .logo').css('display', 'inline-block');
+    $('nav').css('justify-content', 'space-between');
+ } else {
+    $('nav .logo').css('display', 'none');
+    $('nav').css('justify-content', 'flex-end');
+  }
+};
